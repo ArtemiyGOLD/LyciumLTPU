24f04332f236d9a77210d0f7d7f1f127202db36d
ArtemiyGOLD <egolush19@gmail.com>
2025-01-17 21:21:46 +0700

    Commit1

--- src/script.js
+++ src/script.js
@@ -1,22 +1,23 @@
 function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
+    const greenSidebar = document.getElementById('greenSidebar');
+    const blackSidebar = document.getElementById('blackSidebar');
     const welcomeMessage = document.querySelector('.welcome-message');
 
-    if (sidebar.classList.contains('show')) {
-        sidebar.classList.remove('show');
+    if (greenSidebar.classList.contains('show')) {
+        greenSidebar.classList.remove('show');
+        blackSidebar.classList.remove('show');
         setTimeout(() => {
-            sidebar.style.opacity = '0'; // Устанавливаем прозрачность для плавного исчезновения
-        }, 10); // Небольшая задержка для запуска анимации
-        setTimeout(() => {
-            sidebar.classList.add('hidden'); // Скрываем после завершения анимации
-        }, 500); // Убираем после завершения анимации
+            greenSidebar.classList.add('hidden');
+            blackSidebar.classList.add('hidden');
+        }, 500);
         welcomeMessage.style.display = 'block';
     } else {
-        sidebar.classList.remove('hidden');
+        greenSidebar.classList.remove('hidden');
+        blackSidebar.classList.remove('hidden');
         setTimeout(() => {
-            sidebar.style.opacity = '1'; // Устанавливаем видимость для плавного появления
-            sidebar.classList.add('show');
-        }, 10); // Небольшая задержка для запуска анимации
+            greenSidebar.classList.add('show');
+            blackSidebar.classList.add('show');
+        }, 10);
         welcomeMessage.style.display = 'none';
     }
 }
